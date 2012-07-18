dda3a37f26bfc722dc21b86a6652f968f07cf62d
Zhusee Zhang <zhusee2@gmail.com>
2012-07-19 01:28:25 +0800

    Add APIs; implement increaseBySecond() function

--- timer.js
+++ timer.js
@@ -80,8 +80,28 @@ $.extend(NixieClock.prototype, {
       currentActiveDigitElement.removeClass('active');
     }
     targetActiveDigitElement.addClass('active');
-  }
+  },
+  increaseBySecond: function(interval) {
+    if(!interval) interval = 1;
+    
+    var currentTime = this.time,
+        newTime = currentTime;
+    newTime.second = currentTime.second + interval;
+    
+    if(newTime.second > 60) {
+      newTime.minute += Math.floor(newTime.second / 60);
+      newTime.second = newTime.second % 60;
+    }
+    if(newTime.minute > 60) {
+      newTime.hour += Math.floor(newTime.minute / 60);
+      newTime.minute = newTime.minute % 60;
+    }
+    
+    this.setTime(newTime.hour, newTime.minute, newTime.second);
+  },
+  decreaseBySecond: function(interval) {}
 });
+
 
 $(document).ready(function() {
 
