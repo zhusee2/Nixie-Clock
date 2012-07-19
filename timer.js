5a87ed03b2d1de310b8bb6dcdb0bcd224adb34a1
Zhusee Zhang <zhusee2@gmail.com>
2012-07-19 09:50:18 +0800

    Implement decreaseBySecond()

--- timer.js
+++ timer.js
@@ -127,7 +127,24 @@ $.extend(NixieClock.prototype, {
     
     this.setTime(newTime.hour, newTime.minute, newTime.second);
   },
-  decreaseBySecond: function(interval) {}
+  decreaseBySecond: function(interval) {
+    if(!interval) interval = 1;
+    
+    var currentTime = this.time,
+        newTime = currentTime;
+    newTime.second = currentTime.second - interval;
+    
+    if(newTime.second < 0) {
+      newTime.minute += Math.floor(newTime.second / 60);
+      newTime.second = 60 + (newTime.second % 60);
+    }
+    if(newTime.minute < 0) {
+      newTime.hour += Math.floor(newTime.minute / 60);
+      newTime.minute = 60 + (newTime.minute % 60);
+    }
+    
+    this.setTime(newTime.hour, newTime.minute, newTime.second);
+  }
 });
 
 
