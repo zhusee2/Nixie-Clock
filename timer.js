eaa2da18e5ede1ac6b6ab0c4a27e9688cf20c18d
Zhusee Zhang <zhusee2@gmail.com>
2012-07-19 10:48:20 +0800

    Give error when cannot init with specified element

--- timer.js
+++ timer.js
@@ -2,6 +2,7 @@ function NixieClock(target) {
   var nixie = this;
   
   if($(target).length === 0) {
+    console.error('Element “' + target + '” does not exist!');
     return false;
   } else {
     nixie.targetElement = $(target);
